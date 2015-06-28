b6a0ae5dd56f91a40b5a85cd6b2342c1b4d4d058
Viktor Dzundza <victor.dzundza.dev@gmail.com>
2015-06-28 03:53:39 +0300

    use ES6

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,44 +1,31 @@
 
-var React = window.React = require('react'),
-    Timer = require("./ui/Timer"),
-    mountNode = document.getElementById("app");
+var React = window.React = require('react');
+var mountNode = document.getElementById("app");
 
-var TodoList = React.createClass({
-  render: function() {
-    var createItem = function(itemText) {
-      return <li>{itemText}</li>;
-    };
-    return <ul>{this.props.items.map(createItem)}</ul>;
+class Counter extends React.Component{
+  constructor(props){
+    super(props);
+    this.tick = this.tick.bind(this);
+    this.state = {count: props.initialCount};
   }
-});
-var TodoApp = React.createClass({
-  getInitialState: function() {
-    return {items: [], text: ''};
-  },
-  onChange: function(e) {
-    this.setState({text: e.target.value});
-  },
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var nextItems = this.state.items.concat([this.state.text]);
-    var nextText = '';
-    this.setState({items: nextItems, text: nextText});
-  },
-  render: function() {
+
+  tick(){
+    this.setState({count: this.state.count + 1});
+  }
+
+  render(){
     return (
-      <div>
-        <h3>TODO</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.onChange} value={this.state.text} />
-          <button>{'Add #' + (this.state.items.length + 1)}</button>
-        </form>
-        <Timer />
-      </div>
+      <div>Counter clicks {this.state.count} <button onClick={this.tick}>Click here</button></div>
     );
   }
-});
+}
+Counter.propTypes = {initialCount: React.PropTypes.number};
+Counter.defaultProps = {initialCount: 0};
 
+class ProductList extends React.Component{
+  render(){
+    return <p>Hello World ES6 <Counter /></p>;
+  }
+}
 
-React.render(<TodoApp />, mountNode);
-
+React.render(<ProductList></ProductList>, mountNode);
