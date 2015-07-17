14b4f09f021dcab1b855b2eed4c238d939315311
Viktor Dzundza <victor.dzundza.dev@gmail.com>
2015-07-17 22:54:20 +0300

    move row component to another file

--- app/scripts/components/phone-list.react.js
+++ app/scripts/components/phone-list.react.js
@@ -2,44 +2,22 @@ import React from 'react';
 import PhoneStore from '../stores/phone-store.js';
 import {Table} from 'react-bootstrap';
 
-class PhoneItemRow extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {};
-        this.state.phone = props.phone;
-    }
-
-    render() {
-        var phone = this.state.phone;
-        return (
-                  <tr>
-                    <td>{phone.name}</td>
-                    <td>{phone.price}</td>
-                    <td>{phone.attr.color}</td>
-                  </tr>
-              );
-  }
-}
-
-PhoneItemRow.propTypes = {phone: React.PropTypes.object};
-PhoneItemRow.defaultProps = {phone:  {}};
-
+import PhoneRow from '../components/phone-row.react';
 
 class ProductList extends React.Component{
 
-  constructor(props){
+  constructor (props) {
     super(props);
     this.state = {};
     this.state.phones = PhoneStore.getAll();
   }
 
-  renderPhone(phoneItem){
-    return (<PhoneItemRow phone={phoneItem}></PhoneItemRow>);
+  renderPhoneRow (phoneModel) {
+    return (<PhoneRow phone={phoneModel}></PhoneRow>);
     }
 
-    render() {
-      var phoneListItems = this.state.phones.map(this.renderPhone);
+    render () {
+      var phoneRows = this.state.phones.map(this.renderPhoneRow);
       return (
               <Table responsive>
                 <thead>
@@ -49,7 +27,7 @@ class ProductList extends React.Component{
                     <th>color</th> 
                   </tr>
                 </thead> 
-                <tbody>{phoneListItems}</tbody>
+                <tbody>{phoneRows}</tbody>
               </Table>
             );
     }
---
+++ app/scripts/components/phone-row.react.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class PhoneRow extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.state.phone = props.phone;
+    }
+
+    render() {
+        var phone = this.state.phone;
+        return (
+                  <tr>
+                    <td>{phone.name}</td>
+                    <td>{phone.price}</td>
+                    <td>{phone.attr.color}</td>
+                  </tr>
+              );
+  }
+}
+
+PhoneRow.propTypes = {phone: React.PropTypes.object};
+PhoneRow.defaultProps = {phone:  {}};
+
+export default PhoneRow;
