8cfc15ec7a8826b37640e14b946631a30c0fb8e6
Viktor Dzundza <victor.dzundza.dev@gmail.com>
2015-08-03 22:32:45 +0300

    added redirection to app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Router from 'react-router';
 
-import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
+import { DefaultRoute, Link, Route, RouteHandler, Redirect } from 'react-router';
 import { Grid, NavItem, Nav } from 'react-bootstrap';
 import { NavItemLink } from 'react-router-bootstrap';
 
@@ -36,6 +36,7 @@ class App extends React.Component{
 
 let routes = (
   <Route name="app" path="/" handler={App}>
+    <Redirect from="/" to="phoneApp" />
   	<Route name="phoneApp" path="/phones" handler={PhoneApp} />
   	<Route name="phoneItem" path="/phone/:phoneId" handler={PhoneItem} />
   </Route>
