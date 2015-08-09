86551752abcf6f247b9b7de50d4c904a8526895e
Viktor Dzundza <victor.dzundza.dev@gmail.com>
2015-08-09 17:30:51 +0300

    fixes tests, test coveraging, use required instead of import in tests

--- app/__tests__/pagination-test.js
+++ app/__tests__/pagination-test.js
@@ -1,6 +1,18 @@
-jest.dontMock('models/pagination.js');
+jest.dontMock('../scripts/models/pagination.js');
+var Pagination = require('../scripts/models/pagination.js');
+
 describe('Pagination', function() {
+    let pagination;
+
+    beforeEach(function(){
+        pagination = new Pagination;
+    });
+
     it('should be run', function(){
         expect(1 + 1).toBe(2);
+    });
+
+    it('should return 1 when testMethod is invoked ', function(){
+        expect(pagination.test()).toBe(1);
     });
 });
---
+++ app/scripts/models/pagination.js
@@ -0,0 +1,20 @@
+export default class Pagination{
+    constructor(){
+        console.log('constructor run ');
+    }
+
+    test(){
+        return 1;
+    }
+
+    testCoverageUncovered(){
+        for(let i = 0; i < 100; i++){
+            if(i % 2){
+                //do something
+            }else{
+                //do something
+            }
+        }
+    }
+
+}
